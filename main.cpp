d08250030ae012e4d6c1097867f5c19bd6c88d05
AstroThing <andres.rodriguezhn@gmail.com>
2015-11-10 21:16:31 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 10, 2015
+ *      Author: andres
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello world!" << endl;
+}
+
+
+
