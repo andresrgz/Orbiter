2d4b051b4ff5a42d510950128bb576c8afd4ffc0
AstroThing <andres.rodriguezhn@gmail.com>
2015-11-19 23:01:36 -0600

    Gravity works!

--- Entities/Asteroid.cpp
+++ Entities/Asteroid.cpp
@@ -41,7 +41,7 @@ void Asteroid::configure(float x, float y, float mass, string texturePath)
 
 	//Fixture definitions
 	fixtureDef.shape = &asteroidShape;
-	fixtureDef.restitution = 0.5f;
+	fixtureDef.restitution = 0.7f;
 	body->CreateFixture(&fixtureDef);
 
 	drawReady = true;
@@ -49,6 +49,7 @@ void Asteroid::configure(float x, float y, float mass, string texturePath)
 
 void Asteroid::step(vector<Entity*>* entities)
 {
+
 	vector<Entity*> e = *entities;
 	Planet* planet = (Planet*)e[0];
 	b2Body* planetBody = planet->getBody();
@@ -56,17 +57,36 @@ void Asteroid::step(vector<Entity*>* entities)
 	b2Vec2 planetPos = planetBody->GetPosition();
 	b2Vec2 asteroidPos = body->GetPosition();
 
-	float distance = pow((planetPos.x - asteroidPos.x), 2) + pow((planetPos.y - asteroidPos.y), 2);
-	cout << "DISTANCE: " << sqrt(distance) << endl;
+	float deltaX = asteroidPos.x - planetPos.x;
+	float deltaY = asteroidPos.y - planetPos.y;
 
-	float deltaX = planetPos.x - asteroidPos.x;
-	float deltaY = planetPos.y - asteroidPos.y;
-	float angle = atan(deltaY/deltaX);
+	float angle;
+	if(deltaX != 0)
+		angle = atan(deltaY/deltaX);
+	else
+		angle = b2_pi/2.f;
 
+	if(angle < 0)
+		angle*=-1;
+
+	float distance = pow((deltaX), 2) + pow((deltaY), 2);
 	float forceMagnitude = (G * (this->mass) * (planet->mass))/distance;
-	cout << "FORCE :" << forceMagnitude << endl;
+	float forceX = forceMagnitude*cos(angle);
+	float forceY = forceMagnitude*sin(angle);
 
-	b2Vec2 gravityForce(forceMagnitude*cos(angle), forceMagnitude*sin(angle));
+	if(deltaX > 0)
+		forceX*=-1;
+	if(deltaY > 0)
+		forceY*=-1;
+
+	b2Vec2 gravityForce(forceX, forceY);
 
 	body->ApplyForce(gravityForce, body->GetWorldCenter(), true);
+
+	cout << "DELTA X: " << deltaX << " DELTA Y: " << deltaY << endl;
+	cout << "DISTANCE: " << sqrt(distance) << endl;
+	cout << "ANGLE: " << angle*180/b2_pi<< endl;
+	cout << "FORCE MAGNITUDE: " << forceMagnitude << endl;
+	cout << "FORCE X: " << forceMagnitude*cos(angle) << endl;
+	cout << "FORCE Y: " << forceMagnitude*sin(angle) << endl << endl;
 }
--- main.cpp
+++ main.cpp
@@ -34,13 +34,15 @@ int main()
 
 	entities.push_back(new Planet(&world, &window));
 	entities[0]->setScale(0.15f, 0.15f);
-	entities[0]->configure(window.getSize().x/2.f, window.getSize().y/2.f, 1000000000.f, "assets/planets/Planet-8.png");
+	entities[0]->configure(window.getSize().x/2.f, window.getSize().y/2.f, 10000000000.f, "assets/planets/Planet-8.png");
 
 	entities.push_back(new Asteroid(&world, &window));
 	entities[1]->configure(0.f, 0.f, 100.f, "assets/asteroid.png");
 
-	b2Vec2 force(100.f, 0.f);
+	b2Vec2 force(0.25f, 0.f);
 	entities[1]->getBody()->ApplyForce(force, entities[1]->getBody()->GetWorldCenter(), false);
+
+	bool paused = false;
 
 	while(window.isOpen())
 	{
@@ -49,15 +51,23 @@ int main()
 		{
 			if(event.type == Event::Closed)
 				window.close();
+			if(event.type == Event::KeyPressed && event.key.code == Keyboard::Return)
+			{
+				paused = paused ? false : true;
+			}
 		}
 
-		world.Step(1/60.f, 8, 3);
-		((Asteroid*)entities[1])->step(&entities);
+		if(!paused)
+		{
+			world.Step(1/60.f, 8, 3);
+			((Asteroid*)entities[1])->step(&entities);
+			entities[1]->getBody()->ApplyForce(force, entities[1]->getBody()->GetWorldCenter(), false);
 
-		window.clear(Color::Black);
-		entities[0]->draw();
-		entities[1]->draw();
-		window.display();
+			window.clear(Color::Black);
+			entities[0]->draw();
+			entities[1]->draw();
+			window.display();
+		}
 	}
 
 	return 0;
